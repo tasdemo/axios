d10c4383be6d5dba64bc23aff01be428f0d72078
tasdemo <88037462+tasdemo@users.noreply.github.com>
2021-07-28 14:45:10 +0530

    Update http.js

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -58,7 +58,7 @@ describe('supports http with nodejs', function () {
     });
   });
 
-  it.skip('should parse the timeout property', function (done) {
+  it('should parse the timeout property', function (done) {
 
     server = http.createServer(function (req, res) {
       setTimeout(function () {
@@ -87,7 +87,7 @@ describe('supports http with nodejs', function () {
     });
   });
 
-  it.skip('should respect the timeout property', function (done) {
+  it('should respect the timeout property', function (done) {
 
     server = http.createServer(function (req, res) {
       setTimeout(function () {
