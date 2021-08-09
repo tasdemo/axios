4d78de37ee0a0405f7d0b6ac91e50ffad10dcc59
tasdemo <88037462+tasdemo@users.noreply.github.com>
2021-08-09 11:54:36 +0530

    Update http3.js
    RN 3rd try

--- test/unit/adapters/http3.js
+++ test/unit/adapters/http3.js
@@ -970,16 +970,3 @@ describe('supports http with nodejs', function () {
   });
 
 });
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
