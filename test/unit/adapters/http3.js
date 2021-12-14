6309e64be924a1f31b4e7bba36e22f9c702dfcaf
tasdemo <88037462+tasdemo@users.noreply.github.com>
2021-12-14 21:10:49 +0530

    Update http3.js

--- test/unit/adapters/http3.js
+++ test/unit/adapters/http3.js
@@ -325,7 +325,6 @@ describe('supports http with nodejs', function () {
   
   it('[Duplicate 2] should support UTF8', function (done) {
     var str = Array(100000).join('ж');
-    assert.equal("y", str+"x");
     server = http.createServer(function (req, res) {
       res.setHeader('Content-Type', 'text/html; charset=UTF-8');
       res.end(str);
