9af39995c353da9cf332ef2bae19667524c62081
tasdemo <88037462+tasdemo@users.noreply.github.com>
2022-02-01 13:02:15 +0530

    TC2 - update karma conf file

--- karma.conf.js
+++ karma.conf.js
@@ -186,26 +186,22 @@ module.exports = function(config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: browsers,
 
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
-    // Webpack config
+    // Webpack config - Update 1 Feb
     webpack: {
       mode: 'development',
       cache: true,
